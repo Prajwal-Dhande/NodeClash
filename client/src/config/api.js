10a85753d112d28a337ecd6227d2fc52a631601c
Prajwal-Dhande <prajwaldhande017@gmail.com>
2026-05-11 14:34:27 +0530

    Update backend URL

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,3 +1,3 @@
-const API_URL = "http://localhost:5000"; // UNCOMMENT THIS FOR LOCAL DEVELOPMENT
-//const API_URL = "https://codearena-mff0.onrender.com"; // COMMENT THIS OUT
+//const API_URL = "http://localhost:5000"; 
+const API_URL = "https://codearena-mff0.onrender.com"; 
 export default API_URL;
